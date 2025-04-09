9c64469dfc09c76ffea36a1dd560b24c6c33fb36
Rupesh Pal <rupeshpal49@gmail.com>
2025-04-09 22:23:57 +0530

    Add Project-setup code base

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,22 @@
+// import "./components/HomePage.css";
+import "../components/HomePage.css";
+const HomePage = () => {
+  return (
+    <>
+      <div className="navBar bg-blue-400">
+        <div className="logo-section">
+          <img src="#" alt="Logo" />
+        </div>
+        <div className="navList">
+          <a href="#">Home</a>
+          <a href="#">Projects</a>
+          <a href="#">Sites</a>
+          <a href="#">About</a>
+          <a href="#">Contact Us</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
